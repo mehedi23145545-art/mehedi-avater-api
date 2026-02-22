53c590555e067435d245dda5edf4b2ad305e5901
mehedi23145545-art <mehedi23145545@gmail.com>
2026-02-22 16:28:02 +0600

    Add files via upload

--- api/avatar.js
+++ api/avatar.js
@@ -1,34 +1,18 @@
-import crypto from "crypto";
+const crypto = require("crypto");
 
-export default function handler(req, res) {
+module.exports = (req, res) => {
   const { uid } = req.query;
 
   if (!uid) {
     return res.status(400).send("UID parameter required");
   }
 
-  const avatars = [
-    "boy.png",
-    "fire.png",
-    "dragon.png",
-    "cat.png",
-    "ghost.png",
-    "hero.png",
-    "img1.png",
-    "img2.png",
-    "img3.png",
-    "img4.png",
-    "img5.png",
-    "img6.png",
-    "img7.png",
-    "img8.png",
-    "img9.png"
-  ]; // এখানে তোমার আসল 15টা নাম বসাও
+  const totalAvatars = 15;
 
   const hash = crypto.createHash("md5").update(uid).digest("hex");
   const number = parseInt(hash, 16);
 
-  const selected = avatars[number % avatars.length];
+  const avatarNumber = (number % totalAvatars) + 1;
 
-  res.redirect(`/avatars/${selected}`);
-}
+  res.redirect(`/avatars/${avatarNumber}.png`);
+};
